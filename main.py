2763162dd3a3d71d8290fa9d3aa1f62f171c3ae4
b1est <bondarenko2001@gmail.com>
2023-07-31 18:38:34 +0100

    changes

--- main.py
+++ main.py
@@ -26,51 +26,46 @@ def linsearch_worker(alpha):
             pickle.dump(approximations, file)
     return approximations
 
-def create_statistical_materials(text_quantity):
-    for text in range(text_quantity):
-        input_file = Path(f'./saves/materials/pt_{text}.bin')
-        output_file = Path(f'./saves/materials/ct_{text}.bin')
-        with open(input_file, 'wb') as file:
-            file.write(int(text).to_bytes(2, 'little'))
-            file.write(int(text).to_bytes(2, 'little'))
-        
-        
-            file.write(int(text).to_bytes(2, 'little')) 
-        
-        
-        if sys.platform == "linux" or sys.platform == "linux2":
-            Popen(["./heys.bin", "e", "01", input_file, output_file], stdin = PIPE, stderr=True).communicate()
-        elif sys.platform == "darwin":
-            Popen(["./heys.bin", "e", "01", input_file, output_file], stdin = PIPE, stderr=True).communicate()
-        elif sys.platform == "win32":
-            Popen(f"Heys e 01 {input_file} {output_file}", stdin = PIPE, stderr=True).communicate('\n'.encode())
-
 def read(text_quantity):
     texts = []
     for text in range(text_quantity):
         texts.append((int.from_bytes(open(f'./saves/materials/pt_{text}.bin', 'rb').read(), 'little'), int.from_bytes(open(f'./saves/materials/ct_{text}.bin', 'rb').read(), 'little')))
     return texts
 
-def get_last_created_file_and_ab(path: Path or str):
+def create_statistical_materials(text_quantity):
+    if len(os.listdir(Path('./saves/materials')))//2 != config.texts:
+        for text in range(text_quantity):
+            input_file = Path(f'./saves/materials/pt_{text}.bin')
+            output_file = Path(f'./saves/materials/ct_{text}.bin')
+            with open(input_file, 'wb') as file:
+                file.write(int(text).to_bytes(2, 'little')) 
+            if sys.platform == "linux" or sys.platform == "linux2":
+                Popen(["./heys.bin", "e", "01", input_file, output_file], stdin = PIPE, stderr=True).communicate()
+            elif sys.platform == "darwin":
+                Popen(["./heys.bin", "e", "01", input_file, output_file], stdin = PIPE, stderr=True).communicate()
+            elif sys.platform == "win32":
+                Popen(f"Heys e 01 {input_file} {output_file}", stdin = PIPE, stderr=True).communicate('\n'.encode())
+    return read(text_quantity)
+
+def get_saves_m2_and_ab(path: Path or str):
     if isinstance(path, str):
         path = Path(path)
     dir_list = os.listdir(path)
-    if dir_list == []:
+    if not dir_list:
         return None
-    if dir_list:
-        date_list = []
-        for x in dir_list:
-            date_list.append([x, os.path.getctime(path/f'{x}')])
-        sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)
-        last_file = sort_date_list[0][0]
+    else:
+        res = []
         ab = [tuple(map(int, file[:-4].split('-'))) for file in dir_list]
-        return ab, last_file
-    return None
+        for filename in dir_list:
+            with open(path/filename, 'rb') as file:
+                res.append(pickle.load(file))
+        return ab, res
 
 @timeit(display_args=False)
 def m2(args):
     keys = dict()
     alpha, beta, keys_m2, texts = args
+    path_m2_file = Path('./saves/m2') / f"{alpha}-{beta}.plk"
     for k in range(1 << 16):
         u_k = 0
         for x, y in texts:
@@ -81,7 +76,9 @@ def m2(args):
                 u_k -= 1
             keys[k] = abs(u_k)
     _, keys = zip(*sorted(zip(keys.values(), range(1 << 16)), reverse = True))
-    keys_m2.append(keys[:100]) 
+    keys_m2.append(keys[:100])
+    with open(path_m2_file, 'wb') as file:
+        pickle.dump(keys[:100], file)
 
 if __name__ == '__main__':
     if not Path('./saves/approximations').is_dir():
@@ -121,34 +118,24 @@ if __name__ == '__main__':
             with open(Path(f'./saves/approximations/all.pkl'), 'wb') as f:
                     pickle.dump(approximations, f)
     
-    approximations = [(ab[0], ab[1]) for ab, p in sorted(approximations, key = lambda x: x[1], reverse = True)]
-    
-    res = get_last_created_file_and_ab(Path('./saves/m2'))
-
-    
-    if len(approximations) > 300:
-        approximations = approximations[:300]
-
-    last_file = None   
-    if res != None:
-        ab, last_file = res
-        for i in ab:
-            approximations.remove(i)
-
-    if len(os.listdir(Path('./saves/materials')))//2 != config.texts:
-        create_statistical_materials(config.texts)
-
-    texts = read(config.texts)
-
-    num_processes = max(cpu_count()-2, 1)
-    if last_file:
-        keys_m2 = Manager().list(last_file)
-    else:
-        keys_m2 = Manager().list()
-    
-    appr_list_ab = [(ab[0], ab[1], keys_m2, texts) for ab in approximations]
     if not Path('./saves/keys_m2.pkl').exists():
-        
+        approximations = [(ab[0], ab[1]) for ab, p in sorted(approximations, key = lambda x: x[1], reverse = True)]
+        if len(approximations) > 300:
+            approximations = approximations[:300]
+        texts = create_statistical_materials(config.texts)
+        res = get_saves_m2_and_ab(Path('./saves/m2'))
+        keys_m2 = None   
+        if res != None:
+            ab, keys_m2 = res
+            for i in ab:
+                approximations.remove(i)
+        num_processes = max(cpu_count()-2, 1)
+        if keys_m2:
+            keys_m2 = Manager().list(keys_m2)
+        else:
+            keys_m2 = Manager().list()
+        appr_list_ab = [(ab[0], ab[1], keys_m2, texts) for ab in approximations]
+
         with Pool(processes=num_processes) as pool:
             pool.map(m2, appr_list_ab)
         
@@ -156,9 +143,7 @@ if __name__ == '__main__':
             pickle.dump(keys_m2, f)
     else:
         with open(Path('./saves/keys_m2.pkl'), 'rb') as f:
-            data = pickle.load(f)
-        keys_m2 = list()
-        keys_m2 = data
+            keys_m2 = list(pickle.load(f))
         
     keys = dict()
     for k in keys_m2:
@@ -169,5 +154,5 @@ if __name__ == '__main__':
                 keys[ki] = 1
 
     _, candidates = zip(*sorted(zip(keys.values(), range(1 << 16)), reverse = True))
-    logging.info(candidates)
+    logging.info(list(map(hex, candidates)))
     print(candidates[:10])
